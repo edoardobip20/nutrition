61a727f616331addd00b5cf4123bf948a4a698b8
Prato <edoardo.prato@mail-bip.com>
2020-09-17 01:41:02 +0200

    my second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello Project.');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+ 
+console.log('Hello Node.js project.');
+ 
+console.log(process.env.MY_SECRET);
+console.log(process.env.PORT);
+
+const app = express();
+app.use(cors());
+ 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.get('/', (req, res) => {
+return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+
